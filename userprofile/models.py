8c553743e34fa936ebb1cbcf4c44ba590be1b90a
anton <anton.n1996@mail.ru>
2021-10-06 12:40:07 +0500

    1. Изменен тип связи класса UserProfile атрибута user на on_delete=models.CASCADE

--- userprofile/models.py
+++ userprofile/models.py
@@ -5,12 +5,12 @@ class UserProfile(models.Model):
     """
     Абстрактный класс Пользователя
     """
-    user = models.OneToOneField(User, on_delete=models.DO_NOTHING, verbose_name='Пользователь')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Пользователь')
     phone = models.CharField(max_length=12, verbose_name='Телефон')
     points = models.IntegerField(verbose_name='Количество баллов')
     role = models.CharField(max_length=20, verbose_name='Роль')
     rating = models.IntegerField(verbose_name='Рейтинг')
-    code = models.IntegerField(verbose_name='Код')
+    code = models.CharField(max_length=256, verbose_name='Код')
 
     def __unicode__(self):
         return self.user
