d1ef3c01b3ec2f48fe11a0270fe1813953cf2c7a
nahra_000 <anton.n1996@mail.ru>
2021-10-29 02:04:47 +0500

    1. Добавлены дополнительные поля к таблице User: Телефон; Количество баллов; Роль; Рейтинг; Код

--- todo/admin.py
+++ todo/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
 from . import models
 from django.contrib.auth.admin import UserAdmin
-from .models import Status, Task
+from .models import Status, Task, UserProfile
 from django.contrib.auth.models import User
 
 
 admin.site.register(Status)
 admin.site.register(Task)
+
+
+class UserInLine(admin.StackedInline):
+    models = UserProfile
+    can_delete = False
+    verbose_name_plural = 'Доп. информация'
+
+
+class UserAdmin(UserAdmin):
+    inlines = (UserInLine,)
+
+
+# Перерегистрируем модель User
+# admin.site.unregister(User)
+# admin.site.register(User, UserAdmin)
--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
 class Status(models.Model):
@@ -25,3 +26,28 @@ class Task(models.Model):
     status = models.ForeignKey('Status', on_delete=models.DO_NOTHING, default='2', verbose_name='Статус')
     creator_user = models.OneToOneField(User, on_delete=models.DO_NOTHING, verbose_name='Создател')
     # executor_user = models.OneToOneField(User, on_delete=models.DO_NOTHING, verbose_name='Исполнитель')
+
+
+class UserProfile(models.Model):
+    """
+    Дополнительные поля Пользователя
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Пользователь', unique=True)
+    phone = models.CharField(max_length=12, verbose_name='Телефон', unique=True)
+    points = models.IntegerField(verbose_name='Количество баллов', default='0000000', editable=False)
+    role = models.CharField(max_length=20, verbose_name='Роль', default='user', editable=False)
+    rating = models.IntegerField(verbose_name='Рейтинг', default='0000000', editable=False)
+    code = models.CharField(max_length=256, verbose_name='Код', default='0000', editable=False)
+
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
+
+    post_save.connect(create_user_profile, sender=User)
+
+    def __unicode__(self):
+        return self.user_id
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
